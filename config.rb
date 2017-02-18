fdccc72fe59ba7167e719896a99cb157c55b6a50
Alex Collins <alex.e.c@gmail.com>
2017-02-18 21:26:41 +0000

    4

--- config.rb
+++ config.rb
@@ -52,5 +52,4 @@ activate :deploy do |deploy|
   # deploy.port  = 5309 # ssh port, default: 22
   # deploy.clean = true # remove orphaned files on remote host, default: false
   # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
-  deploy.flags = '-avz -o StrictHostKeyChecking=no' 
 end
