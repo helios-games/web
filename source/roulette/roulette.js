0513941f95498ec98e144613ad44532dd7316607
Alex Collins <alex.e.c@gmail.com>
2017-02-20 00:02:46 +0000

    beauty

--- source/roulette/roulette.js
+++ source/roulette/roulette.js
@@ -1,4 +1,8 @@
-var game = new Phaser.Game(800, 450, Phaser.AUTO, 'canvas', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 450, Phaser.AUTO, 'canvas', {
+  preload: preload,
+  create: create,
+  update: update
+});
 
 function preload() {
   game.load.image('bg', '/roulette/images/bg.png');
@@ -10,25 +14,55 @@ var ball;
 // 37 == 00
 // starts at 180 degrees and goes clockwise
 var pockets = [
-  0,28,9,26,30,11,7,20,32,17,5,22,34,15,3,24,36,13,1,37,27,10,25,29,12,8,19,31,18,6,21,33,16,4,23,35,14,2
+  0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, 37, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2
 ];
 var chips = [];
-var wheel = { x: 162, y: 227};
-var wallet = {x:400, y:500};
-var red = {x:462,y:301,r:544,b:329,w:544-462,h:329-301};
-var black = {x:552,y:297,r:628,b:326, w:628-552,h:326-297};
-var numbers = {x:375,y:153,r:715,b:258,w:715-375,h:258-153};
+var wheel = {
+  x: 162,
+  y: 227
+};
+var wallet = {
+  x: 400,
+  y: 500
+};
+var red = {
+  x: 462,
+  y: 301,
+  r: 544,
+  b: 329,
+  w: 544 - 462,
+  h: 329 - 301
+};
+var black = {
+  x: 552,
+  y: 297,
+  r: 628,
+  b: 326,
+  w: 628 - 552,
+  h: 326 - 297
+};
+var numbers = {
+  x: 375,
+  y: 153,
+  r: 715,
+  b: 258,
+  w: 715 - 375,
+  h: 258 - 153
+};
 
 function setPocket(number) {
   console.log("pocket number " + number)
-  $.each(pockets, function(i,v) {
+  $.each(pockets, function(i, v) {
     if (v == number) {
       console.log("i " + i)
       var a = (1 / 4 - (1 - i / pockets.length)) * 2 * Math.PI;
       var r = 75;
       var x = wheel.x + r * Math.cos(a);
-      var y =  wheel.y+ r * Math.sin(a);
-      game.add.tween(ball).to({x:x,y:y},500).start();
+      var y = wheel.y + r * Math.sin(a);
+      game.add.tween(ball).to({
+        x: x,
+        y: y
+      }, 500).start();
     }
   })
 }
@@ -40,53 +74,53 @@ function create() {
 
   var bg = game.add.sprite(game.world.centerX, game.world.centerY, 'bg');
 
- bg.anchor.set(0.5);
- bg.inputEnabled = true;
- bg.events.onInputDown.add(listener, this);
+  bg.anchor.set(0.5);
+  bg.inputEnabled = true;
+  bg.events.onInputDown.add(listener, this);
 
- ball  = game.add.sprite(wheel.x, wheel.y, 'ball');
- ball.anchor.set(0.5);
+  ball = game.add.sprite(wheel.x, wheel.y, 'ball');
+  ball.anchor.set(0.5);
 
- core.setPlayText("Spin!")
- core.updateBalance()
+  core.setPlayText("Spin!")
+  core.updateBalance()
 
- $.getJSON("/api/games/roulette.json", function(data) {
-   setPocket(data.pocket);
-   $.each(data.bets, function(i,bet) {
-     switch (bet.type) {
-       case "number":
-       console.log("bet.pocket " + bet.pocket)
-        var x =  numbers.x + ((bet.pocket -1) / 3 + 0.5) * (numbers.w / 12);
-        var y =  numbers.y + ( 2 - (bet.pocket-1) % 3 + 0.5) * (numbers.h / 3);
+  $.getJSON("/api/games/roulette.json", function(data) {
+    setPocket(data.pocket);
+    $.each(data.bets, function(i, bet) {
+      switch (bet.type) {
+        case "number":
+          console.log("bet.pocket " + bet.pocket)
+          var x = numbers.x + ((bet.pocket - 1) / 3 + 0.5) * (numbers.w / 12);
+          var y = numbers.y + (2 - (bet.pocket - 1) % 3 + 0.5) * (numbers.h / 3);
 
-        addChip(x, y)
-         break;
-         case "red":
-          addChip(red.x+red.w/2, red.y+red.h/2)
-         break;
-         case "black":
-          addChip(black.x+black.w/2, black.y+black.h/2)
-         break;
-     }
-   })
- });
+          chips.push(addChip(x, y));
+          break;
+        case "red":
+          chips.push(addChip(red.x + red.w / 2, red.y + red.h / 2));
+          break;
+        case "black":
+          chips.push(addChip(black.x + black.w / 2, black.y + black.h / 2));
+          break;
+      }
+    })
+  });
 }
 
-function matcher(x1,y1,x2,y2) {
-  return function(x,y) {
+function matcher(x1, y1, x2, y2) {
+  return function(x, y) {
     return x > x1 && x < x2 && y > y1 && y < y2
   }
 }
 
 function listener(sprite, pointer) {
-  console.log(pointer.x+"," +pointer.y)
+  console.log(pointer.x + "," + pointer.y)
   $.each(
     [
-      [matcher(red.x,red.y,red.r,red.b), placeRedBet],
-      [matcher(black.x,black.y,black.r,black.b), placeBlackBet],
-      [matcher(numbers.x,numbers.y,numbers.r,numbers.b), placeNumberBet]
+      [matcher(red.x, red.y, red.r, red.b), placeRedBet],
+      [matcher(black.x, black.y, black.r, black.b), placeBlackBet],
+      [matcher(numbers.x, numbers.y, numbers.r, numbers.b), placeNumberBet]
     ],
-    function(i,v) {
+    function(i, v) {
       var m = v[0](pointer.x, pointer.y);
       if (m) {
         var fn = v[1];
@@ -97,10 +131,13 @@ function listener(sprite, pointer) {
   );
 }
 
-function addChip(x,y) {
-  var chip = game.add.image(wallet.x,wallet.y, "chip")
+function addChip(x, y) {
+  var chip = game.add.image(wallet.x, wallet.y, "chip")
   chip.anchor.set(0.5)
-  game.add.tween(chip).to({x:x,y:y},250).start();
+  game.add.tween(chip).to({
+    x: x,
+    y: y
+  }, 250).start();
   return chip;
 }
 
@@ -121,9 +158,11 @@ function placeBet(type, location, options) {
 function placeBlackBet(pointer) {
   placeBet("black", pointer)
 }
+
 function placeRedBet(pointer) {
   placeBet("red", pointer)
 }
+
 function placeNumberBet(pointer) {
   var x = parseInt(12 * (pointer.x - numbers.x) / numbers.w);
   var y = 3 - parseInt(3 * (pointer.y - numbers.y) / numbers.h);
@@ -131,14 +170,19 @@ function placeNumberBet(pointer) {
 
   console.log("x " + x + ", y " + y + ", number " + number)
 
-  placeBet("number", pointer, {number: number})
+  placeBet("number", pointer, {
+    number: number
+  })
 }
 
-function play () {
-  $.ajax({type: "PUT", url: "/api/games/roulette/spin.json"})
+function play() {
+  $.ajax({
+      type: "PUT",
+      url: "/api/games/roulette/spin.json"
+    })
     .done(function(data) {
-      $.each(chips, function(i,v) {
-        var tween = game.add.tween(v).to(wallet,250)
+      $.each(chips, function(i, v) {
+        var tween = game.add.tween(v).to(wallet, 250)
         tween.onComplete.add(function() {
           v.kill();
         });
@@ -147,8 +191,9 @@ function play () {
       core.setBalance(data.balance);
       setPocket(data.pocket);
     })
-  .fail(function(data) {alert("Failed to spin " + data);});
+    .fail(function(data) {
+      alert("Failed to spin " + data);
+    });
 }
 
-function update() {
-}
+function update() {}
