b3a41ce2038e73804571d374cc98788f4733da72
Alex Collins <alex.e.c@gmail.com>
2017-05-19 23:03:57 +0100

    updated

--- config.rb
+++ config.rb
@@ -16,7 +16,9 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-ignore 'api'
+["classic-slot", "roulette"].each do |name|
+  proxy "/games/#{name}/index.html", "/games/template.html", :locals => { :name => name }, :ignore => true
+end
 
 # General configuration
 
@@ -43,16 +45,4 @@ configure :build do
 
   # Minify Javascript on build
   activate :minify_javascript
-end
-
-activate :deploy do |deploy|
-  deploy.deploy_method = :rsync
-  deploy.host          = 'root@games.alexecollins.com'
-  deploy.path          = '/var/www/html'
-  deploy.user          = 'root'
-  # Optional Settings
-  # deploy.port  = 5309 # ssh port, default: 22
-  # deploy.clean = true # remove orphaned files on remote host, default: false
-  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
-  # deploy.flags = '-avz -e "ssh -i id_rsa"'
 end
